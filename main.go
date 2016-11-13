332d18c523ae4e18ff666aff42f26a7eb1da70aa
Stephen Brennan <smb196@case.edu>
2016-11-13 12:37:14 -0500

    Improved output of main function

--- main.go
+++ main.go
@@ -5,23 +5,38 @@ import (
 	"os"
 )
 
+/*
+A command-line driver for evaluating DPath expressions.
+*/
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("error: must provide DPath expression")
+		return
+	}
+
+	// Parse the DPath expression.
 	tree, err := ParseString(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	// Print out the parse tree.
+	fmt.Println("PARSE TREE:")
 	if err = tree.Print(os.Stdout, 0); err != nil {
 		fmt.Println(err)
+		return
 	}
 
-	fmt.Println("Output:")
-
+	// Evaluate the expression and print the results.
 	ctx := DefaultContext()
 	seq, err := tree.Evaluate(ctx)
 	if err != nil {
+		fmt.Println("ERROR:")
 		fmt.Println(err)
 		return
+	} else {
+		fmt.Println("OUTPUT:")
 	}
 	for seq.Next() {
 		if err = seq.Value().Print(os.Stdout); err != nil {
