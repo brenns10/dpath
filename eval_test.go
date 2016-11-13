38d9f6e3ece0d163a32336f17d2108f05291bacc
Stephen Brennan <smb196@case.edu>
2016-11-13 15:49:28 -0500

    Tests for evaluation of literals

---
+++ eval_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func assertEvaluates(t *testing.T, s string) Sequence {
+	tree, e := ParseString(s)
+	assert.Nil(t, e)
+	ctx := DefaultContext()
+	res, e := tree.Evaluate(ctx)
+	assert.Nil(t, e)
+	return res
+}
+
+func assertSingleton(t *testing.T, seq Sequence) Item {
+	assert.True(t, seq.Next())
+	item := seq.Value()
+	assert.False(t, seq.Next())
+	return item
+}
+
+func TestIntegerLiteral(t *testing.T) {
+	seq := assertEvaluates(t, "1989")
+	item := assertSingleton(t, seq)
+	assert.Equal(t, item.Type(), TYPE_INTEGER)
+	intItem := item.(*IntegerItem)
+	assert.Equal(t, int64(1989), intItem.Value)
+}
+
+func TestDecimalLiteral(t *testing.T) {
+	seq := assertEvaluates(t, "1.234")
+	item := assertSingleton(t, seq)
+	assert.Equal(t, item.Type(), TYPE_DOUBLE)
+	doubleItem := item.(*DoubleItem)
+	assert.Equal(t, 1.234, doubleItem.Value)
+}
+
+func TestFloatLiteral(t *testing.T) {
+	seq := assertEvaluates(t, "1.0e-1")
+	item := assertSingleton(t, seq)
+	assert.Equal(t, item.Type(), TYPE_DOUBLE)
+	doubleItem := item.(*DoubleItem)
+	assert.Equal(t, 1.0e-1, doubleItem.Value)
+}
+
+func TestStringLiteral(t *testing.T) {
+	seq := assertEvaluates(t, "'foo'")
+	item := assertSingleton(t, seq)
+	assert.Equal(t, item.Type(), TYPE_STRING)
+	stringItem := item.(*StringItem)
+	assert.Equal(t, "foo", stringItem.Value)
+}
+
+func TestStringEscapes(t *testing.T) {
+	seq := assertEvaluates(t, "\"bar\"\"\"")
+	item := assertSingleton(t, seq)
+	assert.Equal(t, item.Type(), TYPE_STRING)
+	stringItem := item.(*StringItem)
+	assert.Equal(t, "bar\"", stringItem.Value)
+}
